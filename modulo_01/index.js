efbd417ee5e5c2419dbacac3b8273caff078c4dd
Tiago Vaz <Vaz-Tiago>
2020-01-28 08:20:33 -0300

    Finalizado modulo01

--- modulo_01/index.js
+++ modulo_01/index.js
@@ -7,11 +7,17 @@ const server = express();
 //Dizendo ao express que ele deve utilizar json
 server.use(express.json());
 
-//CRUD
+
+
 
 const users = ['Tiago', 'Cibele', 'Jake'];
 
-//Criando um Middleware Global
+// ##### INICIO MIDDLEWARES #####
+
+
+
+//Middleware Global
+//Global pois sempre será executado antes de qualquer rota (Ótimo para logs)
 server.use((req, res, next) => {
   console.time('Request');
   console.log(`Método: ${req.method}; URL: ${req.url}`);
@@ -23,19 +29,56 @@ server.use((req, res, next) => {
   console.timeEnd('Request')
 });
 
+
+//Middleware Local
+//Verifica se o campo name está vazio ou não existe no body da requisição (POST / PUT)
+function checkUserExist(req, res, next){
+  if(req.body.name === '' || !req.body.name){
+    //res.status retorna o código de status da requisição. No caso o 400 retorna BadRequest
+    return res.status(400).json({ erro: 'User is required!'})
+  }
+  return next()
+}
+
+
+// Middleware Local
+//Verificando se o usuário que foi passado via parametro existe
+function checkUserInArray(req, res, next){
+  //Alterandoa variavel req dentro do middleware
+  const user = users[req.params.index]
+  if(!user){
+    return res.status(400).json({erro: 'User does not exist!'})
+  }
+
+  //Toda rota que utiliza esse middleware terá acesso a variável req.user
+  req.user = user;
+
+  return next();
+}
+
+// ##### FIM MIDDLEWARES #####
+
+
+
 //Listagem de todos usuários
 server.get('/users', (req, res) => {
   return res.json(users);
 });
 
+
+
 //Mostrar um usuário específico
-server.get('/users/:index', (req, res)=> {
-  const { index } = req.params;
-  return res.json({message: `Hello ${users[index]}!!!`});
+server.get('/users/:index', checkUserInArray, (req, res)=> {
+  //Utilizando a variavel criada no middleware
+    //const { index } = req.params;
+    //return res.json(`${users[index]}`);
+  return res.json(req.user);
 });
 
+
+
 //Adicionar um usuário
-server.post('/users', (req, res) => {
+server.post('/users', checkUserExist, (req, res) => {
   const { name } = req.body;
 
   users.push(name);
@@ -43,8 +86,10 @@ server.post('/users', (req, res) => {
   return res.json(users);
 });
 
+
+
 //Editar o usuário:
-server.put('/users/:index', (req, res)=> {
+server.put('/users/:index', checkUserExist, checkUserInArray, (req, res)=> {
   const { index } = req.params;
   const { name } = req.body;
 
@@ -55,12 +100,14 @@ server.put('/users/:index', (req, res)=> {
 
 
 //Deletar usuário
-server.delete('/users/:index', (req, res)=> {
+server.delete('/users/:index', checkUserInArray, (req, res)=> {
   const { index } = req.params;
 
   users.splice(index, 1);
 
   return res.send('');
 });
+
+
 
 server.listen(3000);
