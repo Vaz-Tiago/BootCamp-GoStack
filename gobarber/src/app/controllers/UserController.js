dd2108a1f6e557481daf6357f7b636ea84949d6c
Tiago Vaz <Vaz-Tiago>
2020-02-03 18:53:48 -0300

    data validation with yup

--- gobarber/src/app/controllers/UserController.js
+++ gobarber/src/app/controllers/UserController.js
@@ -1,11 +1,29 @@
+// Biblioteca de validação de dados
+import * as Yup from 'yup';
+
 import User from '../models/User';
 
 class UserController {
   // Função de gravar dados:
   // Basicamanete a mesma estrutura de um middleware.
   async store(req, res) {
-    // Informações vem do corpo da requisição
+    // Regras de validação:
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .required()
+        .min(6),
+    });
 
+    // Verificando validação:
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    // Informações vem do corpo da requisição
     // Verificação se email já está cadastrado:
     const userExists = await User.findOne({ where: { email: req.body.email } });
     if (userExists) {
@@ -23,6 +41,27 @@ class UserController {
   }
 
   async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      // Senha só vai ser requirida se a senha antiga for informada
+      password: Yup.string()
+        .min(6)
+        // Quando ('campo preenchido, funcao se estiver preenchido)
+        //  campo field é ele mesmo
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        // Definindo igualdade dos campos
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
     const { email, oldPassword } = req.body;
 
     // Busca por primary key
