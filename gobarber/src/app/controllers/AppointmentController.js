4a3f54f5615c278c60db0905e8d516d8bccf956b
Tiago Vaz <Vaz-Tiago>
2020-02-17 14:57:09 -0300

    Add appointments validation for past dates and provider Availability

--- gobarber/src/app/controllers/AppointmentController.js
+++ gobarber/src/app/controllers/AppointmentController.js
@@ -1,4 +1,7 @@
 import * as Yup from 'yup';
+
+// date-fns é uma biblioteca que lida com datas no Node.
+import { startOfHour, isBefore, parseISO } from 'date-fns';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 
@@ -13,11 +16,12 @@ class AppointmentController {
       return res.status(400).json({ error: 'Validate fails' });
     }
 
-    const { provider_id, date } = req.body;
+    const providerId = req.body.provider_id;
+    const { date } = req.body;
 
     // Verifica se o provider_id é realemente de um provider
     const isProvider = await User.findOne({
-      where: { id: provider_id, provider: true },
+      where: { id: providerId, provider: true },
     });
 
     if (!isProvider) {
@@ -26,10 +30,37 @@ class AppointmentController {
         .json({ error: 'You can only create appointments with providers' });
     }
 
+    // Define uma hora quebrada como hora inteira
+    const hourStart = startOfHour(parseISO(date));
+
+    // Verificando datas no passado
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past dates are not permited' });
+    }
+
+    // Checar viabilidade da agenda do provider
+
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id: providerId,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkAvailability) {
+      return res
+        .status(400)
+        .json({ error: 'Appointment date is not available' });
+    }
+
+    // Cria o agendamento
     const appointment = await Appointment.create({
       user_id: req.userId,
-      provider_id,
-      date,
+      provider_id: providerId,
+
+      // Define como hora inteira.
+      date: hourStart,
     });
 
     return res.json(appointment);
