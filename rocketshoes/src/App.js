574f5989cd38ad964b7b1ae15702f9563956e269
Tiago Vaz <Vaz-Tiago>
2020-03-20 09:32:30 -0300

    initial styles and routes from rocketshoes

--- rocketshoes/src/App.js
+++ rocketshoes/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import GlobalStyle from './styles/global';
+
+import Routes from './routes';
+import Header from './components/Header';
 
 function App() {
   return (
-    <>
-      <h1>Simple React Web | Ready to code</h1>
-    </>
+    <BrowserRouter>
+      <Header />
+      <GlobalStyle />
+      <Routes />
+    </BrowserRouter>
   );
 }
 
